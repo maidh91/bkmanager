ed1fff31f7ae330306c39dac1578a34d43077ece
bkiter09@gmail.com <bkiter09@gmail.com@dbeafd38-6815-006a-0ef6-98c98132d93b>
2013-07-31 18:37:33 +0000

    Giao diện mới cho thời khóa biểu

--- trunk/BKmanager/src/com/cvteam/bkmanager/adapter/ThoiKhoaBieuAdapter.java
+++ trunk/BKmanager/src/com/cvteam/bkmanager/adapter/ThoiKhoaBieuAdapter.java
@@ -5,35 +5,38 @@ import java.util.List;
 import org.holoeverywhere.widget.TextView;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 
 import com.cvteam.bkmanager.R;
 import com.cvteam.bkmanager.model.DI__ThoiKhoaBieu;
 import com.cvteam.bkmanager.service.LogService;
 
 public class ThoiKhoaBieuAdapter extends BaseAdapter {
-	
+
 	private List<DI__ThoiKhoaBieu> lstTKB;
 	private Context mContext;
 	private LogService logService = new LogService("ThoiKhoaBieuAdapter");
 
-    public ThoiKhoaBieuAdapter(Context context, List<DI__ThoiKhoaBieu> lstTKB) {
-    	logService.functionTag("ThoiKhoaBieuAdapter", "lstTKB.size(): " + lstTKB.size());
-    	mContext = context;
-        this.lstTKB = lstTKB;
-    }
-    
+	public ThoiKhoaBieuAdapter(Context context, List<DI__ThoiKhoaBieu> lstTKB) {
+		logService.functionTag("ThoiKhoaBieuAdapter", "lstTKB.size(): "
+				+ lstTKB.size());
+		mContext = context;
+		this.lstTKB = lstTKB;
+	}
+
 	public void setLstTKB(List<DI__ThoiKhoaBieu> lstTKB) {
-        this.lstTKB = lstTKB;
-    }
-	
+		this.lstTKB = lstTKB;
+	}
+
 	@Override
 	public int getCount() {
 		// TODO Auto-generated method stub
-        return lstTKB.size();
+		return lstTKB.size();
 	}
 
 	@Override
@@ -50,14 +53,42 @@ public class ThoiKhoaBieuAdapter extends BaseAdapter {
 
 	@Override
 	public View getView(int position, View convertView, ViewGroup parent) {
-		logService.functionTag("getView", "position: " + position + " and lstTKB.size(): " + lstTKB.size());
+		logService.functionTag("getView", "position: " + position
+				+ " and lstTKB.size(): " + lstTKB.size());
 		// TODO Auto-generated method stub
 		LayoutInflater inflater = (LayoutInflater) mContext
-		        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		View inflate = inflater.inflate(R.layout.thoikhoabieu_item, parent, false);
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		View inflate = inflater.inflate(R.layout.thoikhoabieu_item, parent,
+				false);
+
+		/*
+		ImageView imageView = (ImageView) inflate
+				.findViewById(R.id.thoikhoabieu_item_icon);
+		if (position % 2 == 0)
+			imageView.setImageResource(R.drawable.thoikhoabieu_item_even);
+		else
+			imageView.setImageResource(R.drawable.thoikhoabieu_item_odd);
+		*/
 
 		TextView txtTenMon = (TextView) inflate.findViewById(R.id.txt_tenmon);
-		TextView txtMaMonHoc = (TextView) inflate.findViewById(R.id.txt_mamonhoc);
+		if (position % 2 == 0)
+			txtTenMon.setBackgroundColor(Color.parseColor("#e0992d"));
+		else
+			txtTenMon.setBackgroundColor(Color.parseColor("#02bd86"));
+
+		TextView txtThu = (TextView) inflate.findViewById(R.id.headerTKB_thu);
+		TextView txtTiet = (TextView) inflate.findViewById(R.id.headerTKB_tiet);
+		TextView txtPhong = (TextView) inflate
+				.findViewById(R.id.headerTKB_phong);
+		if (position % 2 == 0) {
+			txtThu.setBackgroundColor(Color.parseColor("#bc8125"));
+			txtTiet.setBackgroundColor(Color.parseColor("#bc8125"));
+			txtPhong.setBackgroundColor(Color.parseColor("#bc8125"));
+		} else {
+			txtThu.setBackgroundColor(Color.parseColor("#01a775"));
+			txtTiet.setBackgroundColor(Color.parseColor("#01a775"));
+			txtPhong.setBackgroundColor(Color.parseColor("#01a775"));
+		}
 
 		TextView txtThu1 = (TextView) inflate.findViewById(R.id.txt_thu1);
 		TextView txtTiet1 = (TextView) inflate.findViewById(R.id.txt_tiet1);
@@ -66,10 +97,9 @@ public class ThoiKhoaBieuAdapter extends BaseAdapter {
 		TextView txtThu2 = (TextView) inflate.findViewById(R.id.txt_thu2);
 		TextView txtTiet2 = (TextView) inflate.findViewById(R.id.txt_tiet2);
 		TextView txtPhong2 = (TextView) inflate.findViewById(R.id.txt_phong2);
-		    
+
 		DI__ThoiKhoaBieu tkbItem = lstTKB.get(position);
-		txtTenMon.setText(tkbItem.tenmh);
-		txtMaMonHoc.setText(tkbItem.mamh);
+		txtTenMon.setText(tkbItem.mamh + " - " + tkbItem.tenmh);
 
 		// buoi dau tien
 		if (tkbItem.thu1 != 0) {
@@ -92,8 +122,8 @@ public class ThoiKhoaBieuAdapter extends BaseAdapter {
 			txtTiet2.setText("--");
 			txtPhong2.setText("--");
 		}
-		
-        return inflate;
+
+		return inflate;
 	}
 
 }
