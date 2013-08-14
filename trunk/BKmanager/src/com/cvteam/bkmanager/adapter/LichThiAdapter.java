faf470afa076b871dd75361a5b2dc136c2c9794f
duc.huy.ctlk@gmail.com <duc.huy.ctlk@gmail.com@dbeafd38-6815-006a-0ef6-98c98132d93b>
2013-08-14 14:15:00 +0000

    chỉnh lại lịch thi giống như Tkb

--- trunk/BKmanager/src/com/cvteam/bkmanager/adapter/LichThiAdapter.java
+++ trunk/BKmanager/src/com/cvteam/bkmanager/adapter/LichThiAdapter.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.holoeverywhere.widget.TextView;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -48,48 +49,64 @@ public class LichThiAdapter extends BaseAdapter {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		logService.functionTag("getView", "position: " + position
 				+ " and lstlichThi.size(): " + lstlichThi.size());
-		// TODO Auto-generated method stub
+
 		LayoutInflater inflater = (LayoutInflater) mContext
 				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		View inflate = inflater.inflate(R.layout.lichthi_item, parent, false);
-		TextView txtTenMon = (TextView) inflate.findViewById(R.id.txt_tenmon);
-		TextView txtMaMonHoc = (TextView) inflate
-				.findViewById(R.id.txt_mamonhoc);
-		TextView txtGioThiGK = (TextView) inflate
-				.findViewById(R.id.txt_giothigk);
-		TextView txtPhongThiGK = (TextView) inflate
-				.findViewById(R.id.txt_phongthigk);
-		TextView txtGioThiCK = (TextView) inflate
-				.findViewById(R.id.txt_giothick);
-		TextView txtPhongThiCK = (TextView) inflate
-				.findViewById(R.id.txt_phongthick);
 		
-		DI__LichThi lichThiItem = lstlichThi.get(position);
-		txtTenMon.setText(lichThiItem.tenmh);
-        txtMaMonHoc.setText(lichThiItem.mamh);
+		TextView txtTenMon = (TextView) inflate.findViewById(R.id.txt_tenmon);
+		if (position % 2 == 0)
+			txtTenMon.setBackgroundColor(Color.parseColor("#e0992d"));
+		else
+			txtTenMon.setBackgroundColor(Color.parseColor("#02bd86"));
 
-        // final exam info is available
-        if (lichThiItem.tietgk != 0) {
-            txtGioThiGK.setText("  - Ngày " + lichThiItem.ngaygk);
-            txtPhongThiGK.setText("  - Tiết " + lichThiItem.tietgk + ", P." + lichThiItem.phonggk);
-        }
-        else
-        {
-            txtGioThiGK.setText("   --");
-            txtPhongThiGK.setText("   --");
-        }
+		TextView txtNgay = (TextView) inflate.findViewById(R.id.headerLT_ngay);
+		TextView txtTiet = (TextView) inflate.findViewById(R.id.headerLT_tiet);
+		TextView txtPhong = (TextView) inflate
+				.findViewById(R.id.headerLT_phong);
+		if (position % 2 == 0) {
+			txtNgay.setBackgroundColor(Color.parseColor("#bc8125"));
+			txtTiet.setBackgroundColor(Color.parseColor("#bc8125"));
+			txtPhong.setBackgroundColor(Color.parseColor("#bc8125"));
+		} else {
+			txtNgay.setBackgroundColor(Color.parseColor("#01a775"));
+			txtTiet.setBackgroundColor(Color.parseColor("#01a775"));
+			txtPhong.setBackgroundColor(Color.parseColor("#01a775"));
+		}
 
-        // final exam info is not available => show mid-term exam
-        if (lichThiItem.tietck != 0) {
-            txtGioThiCK.setText("  - Ngày " + lichThiItem.ngayck);
-            txtPhongThiCK.setText("  - Tiết " + lichThiItem.tietck + ", P." + lichThiItem.phongck);
-        }
-        else
-        {
-            txtGioThiCK.setText("   --");
-            txtPhongThiCK.setText("   --");
-        }
-        
+		TextView txtNgay1 = (TextView) inflate.findViewById(R.id.txt_ngay_gk);
+		TextView txtTiet1 = (TextView) inflate.findViewById(R.id.txt_tiet_gk);
+		TextView txtPhong1 = (TextView) inflate.findViewById(R.id.txt_phong_gk);
+
+		TextView txtNgay2 = (TextView) inflate.findViewById(R.id.txt_ngay_ck);
+		TextView txtTiet2 = (TextView) inflate.findViewById(R.id.txt_tiet_ck);
+		TextView txtPhong2 = (TextView) inflate.findViewById(R.id.txt_phong_ck);
+
+		DI__LichThi ltItem = lstlichThi.get(position);
+		txtTenMon.setText(ltItem.mamh + " - " + ltItem.tenmh);
+
+		// thi giua ki
+		if (!ltItem.ngaygk.equals("/")) {
+			txtNgay1.setText(ltItem.ngaygk + "");
+			txtTiet1.setText(ltItem.tietgk + "");
+			txtPhong1.setText(ltItem.phonggk);
+		} else {
+			txtNgay1.setText("--");
+			txtTiet1.setText("--");
+			txtPhong1.setText("--");
+		}
+
+		// buoi thu 2
+		if (!ltItem.ngayck.equals("/")) {
+			txtNgay2.setText(ltItem.ngayck + "");
+			txtTiet2.setText(ltItem.tietck + "");
+			txtPhong2.setText(ltItem.phongck);
+		} else {
+			txtNgay2.setText("--");
+			txtTiet2.setText("--");
+			txtPhong2.setText("--");
+		}
+
 		return inflate;
 	}
 
